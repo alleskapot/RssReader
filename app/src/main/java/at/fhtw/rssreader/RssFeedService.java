fbf707ee3ff408ff2a89165b36e6f78c86c4839d
Daniel <if13b053@technikum-wien.at>
2014-10-19 11:33:50 +0200

    [Daniel] - Web Request and return XML as String

--- app/src/main/java/at/fhtw/rssreader/RssFeedService.java
+++ app/src/main/java/at/fhtw/rssreader/RssFeedService.java
@@ -4,23 +4,18 @@ package at.fhtw.rssreader;
  * Created by Marco on 17.10.2014.
  */
 
-        import java.io.IOException;
-        import java.io.InputStream;
-        import java.io.Serializable;
-        import java.net.URL;
-        import java.util.List;
+import android.app.IntentService;
+import android.content.Intent;
 
-        import org.xmlpull.v1.XmlPullParserException;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
-        import android.app.IntentService;
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.os.ResultReceiver;
-        import android.provider.SyncStateContract;
-        import android.util.Log;
-
-        import at.fhtw.rssreader.dataobjects.RssFeed;
-        import at.fhtw.rssreader.dataobjects.RssItem;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 public class RssFeedService extends IntentService {
 
@@ -33,6 +28,30 @@ public class RssFeedService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        //TODO: Parse XML and fill List
+        String xml = getXmlFromUrl("http://www.pcworld.com/index.rss");
+    }
+
+    public String getXmlFromUrl(String url) {
+        String xml = null;
+        try {
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            //XML als String
+            xml = EntityUtils.toString(httpEntity);
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // return XML
+        return xml;
     }
 
 }
